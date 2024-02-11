e66afd2ca22a148a74b5a0f72f91460c9c4fa935
luan torres <luan.torres@sensedata.com.br>
2024-02-11 17:23:42 -0300

    error handler status 404 and 500

--- app/routes.py
+++ app/routes.py
@@ -13,14 +13,21 @@ def get_users():
         user_row = {k: v for k, v in user.__dict__.items() if k != '_sa_instance_state'}
         users_list.append(user_row)
 
-    return make_response(jsonify(users_list))
+    return make_response(jsonify(users_list), 200)
 
 @users_bp.route('/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     user = User.query.filter_by(id=user_id).first()
+
+    if not user:
+        return make_response(
+            jsonify({'msg': f'User with id {user_id} not found.'}),
+            404
+        )        
+
     user_row = {k: v for k, v in user.__dict__.items() if k != '_sa_instance_state'}
 
-    return make_response(jsonify(user_row))
+    return make_response(jsonify(user_row), 200)
 
 @users_bp.route('/', methods=['POST'])
 def add_user():
@@ -33,7 +40,7 @@ def add_user():
     user = User.query.filter_by(email=request_data['email']).first()
     user_row = {k: v for k, v in user.__dict__.items() if k != '_sa_instance_state'}
 
-    return make_response(jsonify(user_row))
+    return make_response(jsonify(user_row), 200)
 
 @users_bp.route('/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
@@ -48,7 +55,7 @@ def update_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     user_row = {k: v for k, v in user.__dict__.items() if k != '_sa_instance_state'}
 
-    return make_response(jsonify(user_row))
+    return make_response(jsonify(user_row), 200)
 
 @users_bp.route('/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
@@ -59,4 +66,16 @@ def delete_user(user_id):
 
     return make_response(jsonify({
         'msg': f'User with id {user_id} has been deleted.'
-    }))
+    }), 200)
+
+@users_bp.app_errorhandler(404)
+def errors_404(e):
+    return make_response(jsonify({
+        'msg': 'Error 404'
+    }), 404)
+
+@users_bp.app_errorhandler(500)
+def errors_500(e):
+    return make_response(jsonify({
+        'msg': 'Internal Server Error'
+    }), 500)
