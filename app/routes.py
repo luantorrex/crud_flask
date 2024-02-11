fedba5f8684db327f9e3e41a48956b4aeb80f7eb
luan torres <luan.torres@sensedata.com.br>
2024-02-11 15:27:34 -0300

    get and put routes working

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,48 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from .models import db, User
+import json
 
 users_bp = Blueprint('users', __name__)
 
-@users_bp.route('/users', methods=['GET'])
+@users_bp.route('/', methods=['GET'])
 def get_users():
-    pass
+    users_table = User.query.all()
+    users_list = []
 
-@users_bp.route('/users/<int:user_id>', methods=['GET'])
+    for user in users_table:
+        user_row = user.__dict__
+        user_row.pop('_sa_instance_state')
+
+        users_list.append(user_row)
+
+    return make_response(jsonify(users_list))
+
+@users_bp.route('/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    pass
+    user = User.query.filter_by(id=user_id).first()
+    user_row = user.__dict__
+    user_row.pop('_sa_instance_state')
 
-@users_bp.route('/users', methods=['POST'])
+    return make_response(jsonify(user_row))
+
+@users_bp.route('/', methods=['POST'])
 def add_user():
-    pass
+    request_data = json.loads(request.data.decode().replace("'", '"'))
+    user = User(username=request_data['username'], email=request_data['email'])
 
-@users_bp.route('/users/<int:user_id>', methods=['PUT'])
+    db.session.add(user)
+    db.session.commit()
+
+    user = User.query.filter_by(email=request_data['email']).first()
+    user_row = user.__dict__
+    user_row.pop('_sa_instance_state')
+
+    return make_response(jsonify(user_row))
+
+@users_bp.route('/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     pass
 
-@users_bp.route('/users/<int:user_id>', methods=['DELETE'])
+@users_bp.route('/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     pass
--- run.py
+++ run.py
@@ -13,7 +13,7 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-    app.register_blueprint(users_bp)
+    app.register_blueprint(users_bp, url_prefix='/users')
 
     if __name__ == '__main__':
         app.run(debug=True)
