abd48ad9fa81b1c4e792048e55fc2e0c44039149
luan torres <luan.torres@sensedata.com.br>
2024-02-11 22:44:25 -0300

    new response

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,12 @@ def get_users() -> str:
     users_table = User.query.all()
     users_json = [user_.to_json() for user_ in users_table]
 
-    return make_response(jsonify(users_json), 200)
+    return make_response(
+        jsonify(
+            message='Listing all users.',
+            data=users_json
+        ), 200
+    )
 
 @users_bp.route('/<int:user_id>', methods=['GET'])
 def get_user(user_id: int) -> str:
@@ -31,7 +36,12 @@ def get_user(user_id: int) -> str:
     user = get_user_by_id(user_id)
     user_json = user.to_json()
 
-    return make_response(jsonify(user_json), 200)
+    return make_response(
+        jsonify(
+            message=f'Listing user with id {user_id}.',
+            data=user_json
+        ), 200
+    )
 
 @users_bp.route('/', methods=['POST'])
 def add_user():
@@ -50,7 +60,12 @@ def add_user():
     user = User.query.filter_by(email=request_data['email']).first()
     user_json = user.to_json()
 
-    return make_response(jsonify(user_json), 200)
+    return make_response(
+        jsonify(
+            message='User created with success.',
+            data=user_json
+        ), 200
+    )
 
 @users_bp.route('/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
@@ -74,7 +89,12 @@ def update_user(user_id):
     user = get_user_by_id(user_id)
     user_json = user.to_json()
 
-    return make_response(jsonify(user_json), 200)
+    return make_response(
+        jsonify(
+            message=f'User with id {user_id} updated with success.',
+            data=user_json
+        ), 200
+    )
 
 @users_bp.route('/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
@@ -89,33 +109,33 @@ def delete_user(user_id):
     db.session.delete(user)
     db.session.commit()    
 
-    return make_response(jsonify({
-        'msg': f'User with id {user_id} has been deleted.'
-    }), 200)
+    return make_response(jsonify(
+        message=f'User with id {user_id} has been deleted.'
+    ), 200)
 
 @users_bp.app_errorhandler(400)
 def errors_400(e):
     """
     In case of not receiving any obligatory field.
     """
-    return make_response(jsonify({
-        'msg': 'Bad Request'
-    }), 400)
+    return make_response(jsonify(
+        message='Bad Request.'
+    ), 400)
 
 @users_bp.app_errorhandler(404)
 def errors_404(e):
     """
     In case of not finding a specific user.
     """
-    return make_response(jsonify({
-        'msg': 'Not Found'
-    }), 404)
+    return make_response(jsonify(
+        message='Not Found.'
+    ), 404)
 
 @users_bp.app_errorhandler(500)
 def errors_500(e):
     """
     Internal server error.
     """
-    return make_response(jsonify({
-        'msg': 'Internal Server Error'
-    }), 500)
+    return make_response(jsonify(
+        message='Internal Server Error.'
+    ), 500)
