000acaa40cf50bf4f54a1e306b5e4e377ae4881d
luan torres <luan.torres@sensedata.com.br>
2024-02-12 10:41:44 -0300

    remmoving factory logic

--- run.py
+++ run.py
@@ -5,15 +5,15 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
     app.register_blueprint(users_bp, url_prefix='/users')
 
-    with app.app_context():
-        db.create_all()
-
-    return app
+    if __name__ == '__main__':
+        app.run(debug=True)
