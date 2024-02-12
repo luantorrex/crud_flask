81f6138f2812a8e5272241418ae8cf56e93ce8f9
luan torres <luan.torres@sensedata.com.br>
2024-02-12 09:22:06 -0300

    run.py as a factory function so we can start dealing with tests

--- run.py
+++ run.py
@@ -5,15 +5,15 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
     app.register_blueprint(users_bp, url_prefix='/users')
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+
+    return app
